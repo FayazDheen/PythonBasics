04e223c675654bc3925d3528202dca1f30f7b344
FayazDheen <40388963+FayazDheen@users.noreply.github.com>
2022-01-23 12:20:03 +0530

    Delete Ex01_km2miles.py

--- Ex01_km2miles.py
+++
@@ -1,5 +0,0 @@
-print("Km to Miles Convertor")
-km = float(input("Enter the Km: ")) #input function always store data as str type
-mile = km/1.609344
-print(km,"Km = ",mile,"miles")
-print (km,"Km = ",round(mile,2),"miles")
