63226c8d044dee584ed48cbc3b46f1119e8eb787
FayazDheen <40388963+FayazDheen@users.noreply.github.com>
2022-01-23 21:28:50 +0530

    Create EX04_circle_area.py

---
+++ Data Type/Numbers/EX04_circle_area.py
@@ -0,0 +1,6 @@
+import math
+radius=float(input("Enter radius: "))
+area = math.pi * (radius ** 2)
+circ = 2 * math.pi * radius
+print("Area = ",round(area,2))
+print("Circumference = ",round(circ,2))
