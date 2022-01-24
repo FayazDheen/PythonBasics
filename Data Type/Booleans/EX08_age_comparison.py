90b2985cdd237a91eb0fb6a1afb050fd8c007b8f
FayazDheen <40388963+FayazDheen@users.noreply.github.com>
2022-01-24 22:19:15 +0530

    Create EX08_age_comparison.py

---
+++ Data Type/Booleans/EX08_age_comparison.py
@@ -0,0 +1,8 @@
+age=22
+userAge = int(input("Enter Your Age"))
+if(userAge > age):
+    print("He's older than you")
+elif(userAge == age):
+    print("You two have the same age")
+else:
+    print("He's younger than you")
